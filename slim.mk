67b9201dd9f447ed071e07f7f6c6a437f28af1fd
sub77 <sub77@ymail.com>
2015-06-24 09:13:40 +0200

    slim -> bliss

--- slim.mk
+++ slim.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/matisselte/full_matisselte.mk)
 
 # Inherit some common slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_tablet_lte.mk)
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := slim_matisselte
 PRODUCT_DEVICE := matisselte
